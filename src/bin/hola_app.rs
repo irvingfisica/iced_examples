a721761fbced46081807cf35f356f036e1f239dd
Irving Morales <irvingfisica@gmail.com>
2020-11-23 10:15:42 -0600

    Círculo con canvas agregado

---
+++ src/bin/hola_app.rs
@@ -0,0 +1,29 @@
+use iced::{Application, executor, Command, Element, Text, Settings};
+
+pub fn main() {
+    Hello::run(Settings::default())
+}
+
+struct Hello;
+
+impl Application for Hello {
+    type Executor = executor::Null;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
+        (Hello, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Hola con aplicación")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hola, mundo!").into()
+    }
+}
